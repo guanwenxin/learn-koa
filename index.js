62cf5ae276d186999671b57599724b02f5515f3c
guanwenxin <764434043@qq.com>
2023-01-31 21:22:16 +0800

    静态网站托管

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
+// // 如果是类或者构造函数，首字母大写
+// const Koa = require('koa')
+// const KoaStatic = require('koa-static')
+// // 创建koa应用
+// const app = new Koa();
+
+// // 1.开发一个静态文件服务器
+// app.use(KoaStatic('./static', {
+//     setHeaders: function(resp) {
+//         // 设置http响应报文key-value，报文的数据保存为附件
+//         resp.setHeader('content-disposition', 'attachment')
+//     }
+// }))
+
+// app.use(async(ctx, next) => {
+//     console.log('1')
+//     // await next();
+//     console.log('2')
+//     await next();
+// })
+// app.use(async(ctx, next) => {
+//     console.log('3')
+//     // await next();
+//     console.log('4')
+//     await next()
+// })
+// app.use(async(ctx, next) => {
+//     console.log('5')
+//     // await next();
+//     console.log('6')
+// })
+// // 处理请求和响应
+// // 中间件函数
+// const middlefn = (ctx) => {
+//     ctx.body = 'hello world'
+// }
+
+// app.use(middlefn)
+
+// // 启动服务器，监听端口
+
+// app.listen(3000, 'localhost', () => {
+//     console.log('server is listening on http://localhost:3000')
+// })
+
 // 如果是类或者构造函数，首字母大写
 const Koa = require('koa')
+const KoaMount = require('koa-mount')
 const KoaStatic = require('koa-static')
 // 创建koa应用
 const app = new Koa();
 
-// 1.开发一个静态文件服务器
-app.use(KoaStatic('./static', {
-    setHeaders: function(resp) {
+// 文件
+const middleStatic = KoaStatic('./static', {
+    setHeaders: function (resp) {
         // 设置http响应报文key-value，报文的数据保存为附件
         resp.setHeader('content-disposition', 'attachment')
     }
-}))
+})
 
-app.use(async(ctx, next) => {
-    console.log('1')
-    // await next();
-    console.log('2')
-    await next();
-})
-app.use(async(ctx, next) => {
-    console.log('3')
-    // await next();
-    console.log('4')
-    await next()
-})
-app.use(async(ctx, next) => {
-    console.log('5')
-    // await next();
-    console.log('6')
-})
-// 处理请求和响应
-// 中间件函数
-const middlefn = (ctx) => {
-    ctx.body = 'hello world'
-}
+// 网站
+const mainSite = KoaStatic('./website')
 
-app.use(middlefn)
+// 1.开发一个静态文件服务器
+app.use(KoaMount('/download', middleStatic))
+app.use(mainSite)
 
 // 启动服务器，监听端口
-
 app.listen(3000, 'localhost', () => {
     console.log('server is listening on http://localhost:3000')
 })
